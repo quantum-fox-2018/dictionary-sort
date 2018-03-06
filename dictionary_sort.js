ee050203bbba526cdd1bddde7fe21da0578d05a2
Natascha Lamsu <tazcha15@gmail.com>
2018-03-06 18:46:17 +0700

    dictionary sort clear

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,20 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
+function sort(arr) {
+  var min = 0;
+  for(var i = 1; i < arr.length; i++) {
+    for(var j = i; j >=0 ; j--) {
+      if(arr[j] < arr[j-1]) {
+        min = arr[j];
+        arr[j] = arr[j-1];
+        arr[j-1] = min;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(sort(["makan", "duduk", "tidur", "terbang"]));
+console.log(sort(["anggi", "angga", "ani", "adi"]));
 
 module.exports = Dictionary
