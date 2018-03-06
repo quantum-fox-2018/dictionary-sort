7b19dbda8b841ed5fb1d77111869aa618fa8457b
barestu <fheartnet@gmail.com>
2018-03-06 19:39:03 +0700

    Update nama fungsi

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,22 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
 
+function Dictionary(arrOfWord) {
+
+  for (let i in arrOfWord) {
+    for (let j in arrOfWord) {
+      if (arrOfWord[i] < arrOfWord[j]) {
+        var temp = arrOfWord[i];
+        arrOfWord[i] = arrOfWord[j];
+        arrOfWord[j] = temp;
+      }
+    }
+  }
+
+  return arrOfWord;
+}
+
+console.log(Dictionary(['makan', 'duduk', 'tidur', 'terbang']));
+console.log(Dictionary(['anggi', 'angga', 'ani', 'adi']));
+
 module.exports = Dictionary
