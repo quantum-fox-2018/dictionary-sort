25a1ceb24a01da294d8d57f0e6eecf7bd9a2cf49
yohanessahrul <yohanessahrul92@gmail.com>
2018-03-06 21:42:25 +0700

    yohanessahrul

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,26 @@
-const readline = require('readline');
+// const readline = require('readline');
 // your code here to initialize the program and take user input
 
-module.exports = Dictionary
+function dictionarySort(value){
+    let newArr = [];
+    for(let i=0; i<value.length; i++){
+        let minValue = value[i];
+        for(let j=i+1; j<value.length; j++){
+            if(value[j] < minValue){
+                idxMin = j;
+                minValue = value[j]
+            }
+        }
+        if(minValue != value[i]){
+            value[idxMin] = value[i]
+            value[i] = minValue;
+        }
+    }
+    return value.join(',');
+}
+
+console.log(dictionarySort(['makan', 'duduk', 'tidur', 'terbang']))
+console.log(dictionarySort(['anggi','angga','ani','adi']))
+
+
+// module.exports = Dictionary
