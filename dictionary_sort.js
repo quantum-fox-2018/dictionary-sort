cae4d386f1375e72d286299b6b364ec22a504aa0
Hacktiv8 <hacktiv8@H8JKT003.local>
2018-03-06 21:34:41 +0700

    dictionary

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,21 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
 
-module.exports = Dictionary
+function dictionary(array) {
+  for (var i = 0; i < array.length; i++) {
+    for (var j = i; j < array.length; j++) {
+      if (array[j] < array[i]) {
+        var temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
+      }
+    }
+  }
+  return array.join(', ')
+}
+
+console.log(dictionary(['makan', 'duduk', 'tidur', 'terbang']))
+console.log(dictionary(['anggi', 'angga', 'ani', 'adi']))
+
+
+// module.exports = Dictionary
