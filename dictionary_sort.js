59044e94c378dbdd8eb1e3eb74dd8236bf70f6a6
ekifakhrureza <ekifakhrureza@gmail.com>
2018-03-06 16:15:26 +0700

    DICTIONARY SORT

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,4 +1,20 @@
 const readline = require('readline');
 // your code here to initialize the program and take user input
+function Dictionary(array){
+  var tempWord
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 0; j < array.length; j++) {
+      if(array[j]>array[j+1]){
+        tempWord=array[j+1].toUpperCase()
+        array[j+1]=array[j].toUpperCase()
+        array[j]=tempWord.toUpperCase()
+      }
+    }
+  }
+  console.log(array);
+}
+
+Dictionary(['makan','duduk','tidur','terbang'])
+Dictionary(['anggi','angga','ani','adi'])
 
 module.exports = Dictionary
